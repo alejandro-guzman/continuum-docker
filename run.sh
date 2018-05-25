e035a38cce0a20ca86304c1d8786b0882af84589
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-05-25 15:21:10 -0400

    S-65143 Add comments and remove installer from dockercompose.

--- run.sh
+++ run.sh
@@ -7,17 +7,19 @@ sleep 5s
 # Make sure ENV is set correctly
 source ~/.profile
 export CONTINUUM_HOME=/opt/continuum/current
-#while true; do sleep 10s; done
+#while true; do sleep 10s; done  # Debugging
 
 # Setup up DB
 if [ ! -f "/var/continuum/db_configured" ]; then
     echo "Setting up Continuum DB..."
+    # Run DB initialization
     /opt/continuum/python/bin/python \
         /opt/continuum/current/common/install/init_mongodb.py -p "n813KLVh7sLowt08A66tEQ=="
+    # Set flag for DB configured so as to not configure more that once.
     touch /var/continuum/db_configured
 fi
 
-# Start CTM
+# Start Continuum services
 ctm-restart-services
-# Keeps container from exiting
+# Keep container from exiting
 while true; do sleep 10s; done
