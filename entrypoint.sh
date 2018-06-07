ed85ff0d355b0cad6393b3f4b1fcca84b6501b3c
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-06-07 14:35:38 -0400

    S-65309 remove existing database setting (rely on env vars)

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,9 @@ fi
 # Setup up DB
 if [ -z ${SKIP_DB+x} ]; then
     echo "Setting Continuum DB"
+    # Remove mongodb_database setting from config file, environment variables passed in will handle mongo settings
+    sed -i '/mongodb_database/d' /etc/continuum/continuum.yaml
+
     DEFAULT_ADMIN_PASSWORD="n813KLVh7sLowt08A66tEQ=="  # "password"
     ${CONTINUUM_HOME}/common/updatedb.py || \
         ${CONTINUUM_HOME}/common/install/init_mongodb.py --password $DEFAULT_ADMIN_PASSWORD
