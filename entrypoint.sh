50cc70237393284f394a9f6393876e0d4868fbfb
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-06-07 14:37:42 -0400

    S-65309 run init or upgrade (REFACTOR THIS to always run upgrade and if detects nothing then init.)

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,8 +19,8 @@ if [ -z ${SKIP_DB+x} ]; then
     sed -i '/mongodb_database/d' /etc/continuum/continuum.yaml
 
     DEFAULT_ADMIN_PASSWORD="n813KLVh7sLowt08A66tEQ=="  # "password"
-    ${CONTINUUM_HOME}/common/updatedb.py || \
-        ${CONTINUUM_HOME}/common/install/init_mongodb.py --password $DEFAULT_ADMIN_PASSWORD
+    ${CONTINUUM_HOME}/common/install/init_mongodb.py --password $DEFAULT_ADMIN_PASSWORD || ${CONTINUUM_HOME}/common/updatedb.py
+    echo "Done setting Continuum DB"
 fi
 
 # https://stackoverflow.com/questions/39082768/what-does-set-e-and-exec-do-for-docker-entrypoint-scripts?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
