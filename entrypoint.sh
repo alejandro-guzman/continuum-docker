49c9abe73c17086fe9cfda4888ec07e15a5ae2bc
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-06-08 15:00:28 -0400

    D-13678 Shelf file is removed to avoid restart issue.

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,5 +23,11 @@ if [ -z ${SKIP_DB+x} ]; then
     echo "Done setting Continuum DB"
 fi
 
+# File corruption always causing login issues.
+shelf_file=/var/continuum/ui/cskuisession.shelf
+if [ -f $shelf_file ]; then
+    rm -f $shelf_file
+fi
+
 # https://stackoverflow.com/questions/39082768/what-does-set-e-and-exec-do-for-docker-entrypoint-scripts?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 exec "$@"
