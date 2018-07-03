533895f61dc5cc0b4cc279c50b53cbb7b460c859
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-07-03 00:32:49 -0400

    WIP specifiying libs. docs.

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+echo "entrypoint.."
+
+while true; do sleep 1; done
+
+exec "$@"
--- dev/start.sh
+++ dev/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+while true; do sleep 1; done
 ${CONTINUUM_HOME}/client/bin/ctm-start-services && \
 tail -f /var/continuum/log/ctm-ui.log
