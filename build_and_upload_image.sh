8fd3476d7429cfcd3c3261ec0b042ec852170cf8
Alejandro Guzman <a.guillermo.guzman@gmail.com>
2018-08-03 01:24:11 -0400

    Refine build and release script.

--- build_and_upload_image.sh
+++ build_and_upload_image.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
+set -ex
 
-set -e
 
-INSTALLER_LINK=$1
+USERNAME="continuumserviceuser"
+ORGANIZATION="cycletime"
+IMAGE="continuum"
 
-docker image build \
-    --tag continuum-prod \
-    --build-arg INSTALLER=${INSTALLER_LINK} \
-    --file Dockerfile \
-    .
-REPO=$2  # ex: "foo.dkr.ecr.us-east-1.amazonaws.com/continuum"
-TAG=$3  # defaults to latest
 
-docker image tag continuum-prod:latest ${REPO}:${TAG}
-docker push ${REPO}
+echo "[INFO] Logging into Docker using username ${USERNAME}"
+cat ${HOME}/.docker/continuumserviceuser-pw | docker login -u ${USERNAME} --password-stdin
+
+
+# Tag will be the version.revision-story_number
+version=$(grep -oP "(\d{2}\.\d\.\d*\.\d*-\w-\d{5})" ./docker-compose.yml)
+[ -z ${version} ] && (echo "[ERROR] Could not determine image version" && exit 1)
+
+
+link=$(grep -oP "(https.*installer\.sh)" ./docker-compose.yml)
+[ -z ${link} ] && (echo "[ERROR] Could not determine image installer link" && exit 1)
+
+
+docker image build --tag ${IMAGE}:latest --build-arg INSTALLER=${link} ${PWD}
+
+
+echo "[INFO] Tagging image ${IMAGE} to repo ${ORGANIZATION}/${IMAGE} with ${version}"
+docker image tag ${IMAGE}:latest ${ORGANIZATION}/${IMAGE}:${version}
+
+
+echo "[INFO] Pushing image to repo ${ORGANIZATION}"
+docker push ${ORGANIZATION}/${IMAGE}:latest
+docker push ${ORGANIZATION}/${IMAGE}:${version}
+
+
+docker logout
+exit 0
